1c2ccb2aeb5fe9d888afe0d1ee40b95f0618a530
Swuan Yee <k.swuan.yee@gmail.com>
2024-02-19 15:54:14 +0630

    changed the postgres code so that it adds new data rather than change existing data

--- store_rates.py
+++ store_rates.py
@@ -23,7 +23,7 @@ class CurrencyRates(Base):
     entry_time = Column(String)
     rate_date = Column(String)
     rate_time = Column(String)
-    currency = Column(String, unique=True)
+    currency = Column(String)
     face_2_face = Column(Float)
     buy = Column(Float)
     sell = Column(Float)
@@ -59,40 +59,25 @@ def store_exchange_rates():
     wb = load_workbook(excel_file, data_only=True)
     sheet = wb[sheet_name]
 
-    # Fetch and format the entry date and time
     entry_date = datetime.now().date().strftime('%Y-%m-%d')
     entry_time = datetime.now().time().strftime('%H:%M:%S')
-
-    # Fetch the rate date and time
     rate_date = sheet['F2'].value.strftime('%Y-%m-%d')
     rate_time = sheet['H2'].value.strftime('%H:%M:%S')
 
     for currency, row in currency_rows.items():
         rates = [sheet[f'{col}{row}'].value for col in transactions]
 
-        # Attempt to find an existing record for this currency
-        currency_rate = session.query(CurrencyRates).filter_by(currency=currency).first()
+        # Always create a new record
+        new_currency_rate = CurrencyRates(entry_date=entry_date, entry_time=entry_time,
+                                          rate_date=rate_date, rate_time=rate_time,
+                                          currency=currency, face_2_face=rates[0],
+                                          buy=rates[1], sell=rates[2], visa=rates[3],
+                                          export=rates[4], remittance=rates[5], usdt=rates[6])
+        session.add(new_currency_rate)
 
-        # If found, update existing record
-        if currency_rate:
-            currency_rate.entry_date, currency_rate.entry_time, \
-                currency_rate.rate_date, currency_rate.rate_time, \
-                currency_rate.face_2_face, currency_rate.buy, currency_rate.sell, \
-                currency_rate.visa, currency_rate.export, currency_rate.remittance, \
-                currency_rate.usdt = entry_date, entry_time, rate_date, rate_time, *rates
-        else:
-            # Otherwise, create a new record
-            new_currency_rate = CurrencyRates(entry_date=entry_date, entry_time=entry_time,
-                                              rate_date=rate_date, rate_time=rate_time,
-                                              currency=currency, face_2_face=rates[0],
-                                              buy=rates[1], sell=rates[2], visa=rates[3],
-                                              export=rates[4], remittance=rates[5], usdt=rates[6])
-            session.add(new_currency_rate)
-
-    # Save changes to the database
     session.commit()
     session.close()
-    print("Exchange rate information has been updated in the database.")
+    print("Exchange rate information has been added to the database.")
 
 
 store_exchange_rates()
